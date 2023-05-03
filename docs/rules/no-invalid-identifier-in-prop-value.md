cac5065549654bdfb96c119e242b76aab01a9aba
amir ali <amir.ali@regentmarkets.com>
2023-05-03 15:50:45 +0500

    feat: add rule `no-invalid-identifier-in-prop-value`

---
+++ docs/rules/no-invalid-identifier-in-prop-value.md
@@ -0,0 +1,37 @@
+# Validate string literal passed to Localize component in i18n_default_text prop is valid and has all the identifiers in values prop (no-invalid-identifier-in-prop-value)
+
+## Rule Details
+
+Examples of **incorrect** code for this rule:
+
+```js
+<Localize i18n_default_text="You cannot use your real money account {{website_name}} with at this time." />;
+
+<Localize i18n_default_text="You cannot use your real money account {{variable_name}} with at this time." />;
+
+<Localize
+  i18n_default_text="You cannot use your real money account {{variable_name}} with at this time."
+  values={{ website_name }}
+/>;
+```
+
+Examples of **correct** code for this rule:
+
+```js
+<Localize
+  i18n_default_text="You cannot use your real money account with {{website_name}} at this time."
+  values={{ website_name }}
+/>;
+
+<Localize i18n_default_text="You cannot use your real money account with at this time." />;
+
+<Localize
+  i18n_default_text={`You cannot use your real money account with at this time.`}
+/>;
+```
+
+### Options
+
+```
+...
+```
---
+++ tests/lib/rules/no-invalid-identifier-in-prop-value.js
@@ -0,0 +1,78 @@
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const { RuleTester } = require("eslint");
+const rule = require("../../../lib/rules/no-invalid-identifier-in-prop-value");
+
+const parsers = require("../../helpers/parsers");
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ERROR_MESSAGE = "Identifier value is not passed in values";
+
+const ruleTester = new RuleTester({ parser: parsers.BABEL_ESLINT });
+ruleTester.run("no-invalid-identifier-in-prop-value", rule, {
+  valid: [
+    {
+      code: `
+        <Localize
+            i18n_default_text="You cannot use your real money account with {{website_name}} at this time."
+            values={{ website_name }}
+        />;
+    `,
+    },
+    {
+      code: `
+        <Localize i18n_default_text="You cannot use your real money account with at this time." />
+      `,
+    },
+    {
+      code:
+        "<Localize i18n_default_text={`You cannot use your real money account with at this time.`} />",
+    },
+  ],
+
+  invalid: [
+    {
+      code: `<Localize i18n_default_text="You cannot use your real money account {{website_name}} with at this time." />`,
+      errors: [
+        {
+          message: ERROR_MESSAGE,
+          line: 1,
+          column: 2,
+          type: "JSXIdentifier",
+        },
+      ],
+    },
+    {
+      code: `<Localize i18n_default_text="You cannot use your real money account {{variable_name}} with at this time." />`,
+      errors: [
+        {
+          message: ERROR_MESSAGE,
+          line: 1,
+          column: 2,
+          type: "JSXIdentifier",
+        },
+      ],
+    },
+    {
+      code: `
+        <Localize
+            i18n_default_text="You cannot use your real money account {{variable_name}} with at this time."
+            values={{ website_name }}
+        />;
+      `,
+      errors: [
+        {
+          message: ERROR_MESSAGE,
+          line: 2,
+          column: 10,
+          type: "JSXIdentifier",
+        },
+      ],
+    },
+  ],
+});
